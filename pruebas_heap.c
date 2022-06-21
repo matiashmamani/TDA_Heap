5b4b853edb199aeda5ce3a9e22a2b2f9232661be
Matias <matias.h.mamani@gmail.com>
2022-06-21 01:11:19 -0300

    20/06 Pruebas faltantes

--- pruebas_heap.c
+++ pruebas_heap.c
@@ -1,6 +1,7 @@
-#define VOLUMEN 100000
+#define VOLUMEN 10000
 
 #include "heap.h"
+#include "pila.h"
 #include "testing.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -35,7 +36,7 @@ static void prueba_heap_elementos_estaticos(void) {
     int n_int = 10;
     int *p_int;
     
-    printf("2. INICIO DE PRUEBAS CON HEAP DE ELEMENTOS ESTATICOS\n");
+    printf("3. INICIO DE PRUEBAS CON HEAP DE ELEMENTOS ESTATICOS\n");
     heap_t *heap = heap_crear(intcmp);
     print_test("- Crear heap", heap);
 
@@ -87,7 +88,7 @@ static void prueba_heap_elementos_dinamicos(void){
         *(vec_int[i]) = i;
     }
 
-    printf("3. INICIO DE PRUEBAS CON HEAP DE ELEMENTOS DINAMICOS\n");
+    printf("4. INICIO DE PRUEBAS CON HEAP DE ELEMENTOS DINAMICOS\n");
     heap_t *heap = heap_crear_arr((void**)vec_int, n_int, intcmp);
     print_test("- Crear heap", heap);
     print_test("- Heap no esta vacío", !heap_esta_vacio(heap));
@@ -101,7 +102,7 @@ static void prueba_heap_elementos_dinamicos(void){
 
 static void prueba_heap_volumen(void) {
 
-    printf("4. INICIO DE PRUEBAS DE VOLUMEN\n");
+    printf("7. INICIO DE PRUEBAS DE VOLUMEN\n");
     heap_t *heap = heap_crear(intcmp);
     print_test("- Crear heap", heap);
     
@@ -156,7 +157,7 @@ static void prueba_heap_sort(void) {
         *(vec_int[i]) = vec_desord[i];
     }
 
-    printf("5. INICIO DE PRUEBAS HEAP SORT\n");
+    printf("8. INICIO DE PRUEBAS HEAP SORT\n");
     print_test("- Vector dinámico creado. Todos los elementos están desordenados", true);
     
     heap_sort((void**)vec_int, n_int, intcmp);
@@ -174,10 +175,122 @@ static void prueba_heap_sort(void) {
     print_test("- Vector dinámico destruido", true);
 }
 
+int nullcmp(const void *a, const void *b){
+    return 0;
+}
+
+static void prueba_heap_valor_null(void) {
+
+    void **vec_null;
+    int n_null = 4;
+
+    // Creo vector de NULLs
+    vec_null = malloc((size_t)n_null * sizeof(void*));
+    for(int i = 0; i < n_null; i++){
+        vec_null[i] = NULL;
+    }
+ 
+    printf("2. INICIO DE PRUEBAS CON VALOR NULL\n");
+    heap_t *heap = heap_crear(nullcmp);
+    print_test("- Crear heap", heap);
+
+    bool ok = true;
+    bool ok_ver_max = true;
+    void **p_null;
+    for(int i = 0; i < n_null; i++){
+        ok &= heap_encolar(heap, &vec_null[i]);
+        p_null = heap_ver_max(heap);
+        ok_ver_max &= (*p_null == NULL);
+    }
+    print_test("- Se pudieron encolar todos los elementos", ok);
+    print_test("- Ver max es NULL en cada encolada", ok_ver_max);
+    print_test("- heap no esta vacío", !heap_esta_vacio(heap));
+
+    ok = true;
+    p_null = NULL;
+    for(int i = n_null - 1; i >= 0; i--){
+        p_null = heap_desencolar(heap);
+        ok &= (*p_null == vec_null[i]);
+    }
+    print_test("- Se pudieron desencolar todos los elementos", ok);
+    print_test("- heap quedó vacío después de desencolar todo", heap_esta_vacio(heap));
+    print_test("- Ver max de heap devuelve NULL después de desencolar todo", !heap_ver_max(heap));
+    print_test("- Desencolar en heap vacío devuelve NULL", !heap_desencolar(heap));
+    heap_destruir(heap, NULL);
+    
+    // Destruyo vector de NULLs
+    free(vec_null);
+}
+
+void pila_destruir_wrapper(void *pila){
+    pila_destruir((pila_t*)pila);
+}
+
+static void prueba_heap_funcion_destruir(void){
+
+    printf("6. INICIO DE PRUEBAS CON FUNCION DESTRUIR (PILA_DESTRUIR)\n");
+    heap_t* heap = heap_crear(nullcmp);
+    size_t largo = 10;
+    pila_t *pila = NULL;
+
+    /* Inserta 'largo' parejas en el heap */
+    bool ok = true;
+    for(unsigned i = 0; i < largo; i++) {
+        pila = pila_crear();
+        ok = heap_encolar(heap, pila);
+        if(!ok) break;
+    }
+
+    print_test("- Se pudieron almacenar varias pilas", ok);
+    print_test("- La cantidad de pilas almacenadas es correcta", heap_cantidad(heap) == largo);
+    print_test("- Destruir con funcion de destruccion pila_destruir", true);
+
+    /* Destruye el heap con funcion pila_destruir */
+    heap_destruir(heap, pila_destruir_wrapper);
+}
+
+static void prueba_heap_crear_arr(void) {
+
+    int **vec_int;
+    size_t n_int  = 3;
+    int *p_int;
+
+    vec_int = malloc(n_int * sizeof(int*));
+    for(int i = 0; i < n_int; i++){
+        vec_int[i] = malloc(sizeof(int));
+        *(vec_int[i]) = i;
+    }
+
+    printf("5. INICIO DE PRUEBAS CON HEAP CREADO DESDE ARREGLO\n");
+    heap_t *heap = heap_crear_arr((void**)vec_int, n_int, intcmp);
+    print_test("- Crear heap desde arreglo", heap);
+    print_test("- Heap no esta vacío", !heap_esta_vacio(heap));
+    print_test("- La cantidad de elementos del heap es correcta", heap_cantidad(heap) == n_int);
+
+    bool ok = true;
+    for(int i = 0; i < n_int; i++){
+        p_int = heap_ver_max(heap);
+        ok &= (p_int == heap_desencolar(heap));
+        free(p_int);
+    }
+
+    print_test("- Se pudieron desencolar todos los elementos y mantienen invariante de heap", ok);
+    print_test("- Heap quedó vacío después de desencolar todo", heap_esta_vacio(heap));
+    print_test("- La cantidad de elementos del heap es correcta", !heap_cantidad(heap));
+    print_test("- Ver máximo de heap devuelve NULL después de desencolar todo", !heap_ver_max(heap));
+    print_test("- Desencolar en heap vacío devuelve NULL", !heap_desencolar(heap));
+    heap_destruir(heap, free);
+    print_test("- Destruir heap (con free)", true);
+    free(vec_int);
+}
+
 void pruebas_heap_estudiante() {
     prueba_heap_vacia();
+    prueba_heap_valor_null();
     prueba_heap_elementos_estaticos();
     prueba_heap_elementos_dinamicos();
+    prueba_heap_crear_arr();
+    prueba_heap_funcion_destruir();
     prueba_heap_volumen();
     prueba_heap_sort();
 }
